7ab8bb4915e01a990220ea1e78d1f6875f3fbee0
MVRCK <domikrau@gmail.com>
2024-09-21 01:50:36 +0200

    Befehle

--- src/plugins/Console/server/index.ts
+++ src/plugins/Console/server/index.ts
@@ -12,3 +12,28 @@ Messenger.commands.register({
         rPlayer.notify.showNotification('Test befehl!');
     },
 });
+Messenger.commands.register({
+    name: 'giveweapon',
+    desc: '- /giveweapon (weaponid)',
+    callback: async (player, ...args: string[]) => {
+        const rPlayer = Rebar.usePlayer(player);
+        player.giveWeapon(args[0], 50, true);
+        rPlayer.notify.showNotification('Waffe wurde erfolgreich gegeben!');
+    },
+});
+Messenger.commands.register({
+    name: 'car',
+    desc: '- /car (carid)',
+    callback: async (player, ...args: string[]) => {
+        const rPlayer = Rebar.usePlayer(player);
+        const pos = player.pos;
+        const vehicle = new alt.Vehicle(
+            args[0],
+            new alt.Vector3(pos.x + 2.5, pos.y + 2.5, pos.z),
+            new alt.Vector3(0, 0, 0),
+        );
+        vehicle.numberPlateText = 'Test';
+        vehicle.engineOn = true;
+        rPlayer.notify.showNotification('Fahrzeug wurde erfolgreich gegeben!');
+    },
+});
